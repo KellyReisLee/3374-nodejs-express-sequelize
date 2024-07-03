a3be55c6494c6214206d1fa80123337c8270ae8d
Kelly Reis Lee <kellyreislee@MacBook-Pro-de-Kelly.local>
2024-07-03 16:52:49 +0100

    Adding association between tables

---
+++ src/migrations/20240703101642-create-curso.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cursos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        type: Sequelize.STRING
+      },
+      descricao: {
+        type: Sequelize.STRING
+      },
+      data_inicio: {
+        type: Sequelize.DATEONLY
+      },
+      docente_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        // 'model' é só o nome da propriedade - Aqui passamos o nome da tabela associada.
+        // 'key' passamos o nome da coluna que vai fazer a associação.
+        references: { model: 'pessoas', key: 'id' }
+
+      },
+      categoria_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        // 'model' é só o nome da propriedade - Aqui passamos o nome da tabela associada.
+        // 'key' passamos o nome da coluna que vai fazer a associação.
+        references: { model: 'categorias', key: 'id' }
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cursos');
+  }
+};
